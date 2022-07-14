2e41d6961eba94a056b1e90d6c352d427ccc89c9
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-07-14 10:46:53 -0400

    cleaned up the code using prettier

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require("path")
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({helpers});
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
 const session = require("express-session");
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SequelizeStore = require("connect-session-sequelize")(session.Store)
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sess = {
   secret: "Super secret secret",
   cookie: {},
   resave: false,
-  saveUninitialized: true, 
+  saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
-}
+    db: sequelize,
+  }),
+};
 
 app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars")
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
 
 // turn on routes
@@ -33,5 +33,5 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
